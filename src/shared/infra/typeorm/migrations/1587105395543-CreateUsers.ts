1f261263bae2e92a668363268006103e041ac049
Georg Augusto Schlegel <georgaugusto@gmail.com>
2020-09-28 23:17:46 -0300

    changing names

--- src/shared/infra/typeorm/migrations/1587105395543-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1587105395543-CreateUsers.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateUsers1588105395543 implements MigrationInterface {
+export default class CreateUsers1587105395543 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
